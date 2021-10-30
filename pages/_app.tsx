8b451f7316d7aa2e1e5cc4cb7a5d2511934f9e60
Ulisses <m.stunik@gmail.com>
2021-10-30 20:43:33 -0300

    add icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+    <title>Plantes</title>
+
+    {/* icon */}
+    <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png"/>
+    <link rel="manifest" href="/icon/site.webmanifest"/>
+    <link rel="mask-icon" href="/icon/safari-pinned-tab.svg" color="#5bbad5"/>
+    <meta name="msapplication-TileColor" content="#00a300"/>
+    <meta name="theme-color" content="#ffffff"/>
+  </Head>
+  <Component {...pageProps} /></>
 }
 
 export default MyApp
