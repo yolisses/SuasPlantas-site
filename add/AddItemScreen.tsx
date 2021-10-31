47b5f0f9be9c5a5eb83dc159e19be0bc86168ea2
Ulisses <m.stunik@gmail.com>
2021-10-31 07:51:56 -0300

    add emphasis button

--- add/AddItemScreen.tsx
+++ add/AddItemScreen.tsx
@@ -1,10 +1,12 @@
+import { EmphasisButton } from "./EmphasisButton";
 import { TextInput } from "./TextInput";
 
 export function AddScreen() {
   return (
-    <div className="flex flex-col p-2">
+    <div className="flex flex-col flex-1 h-full items-stretch p-2">
       <TextInput label="Nome" />
       <TextInput label="Descrição" optional />
+      <EmphasisButton text="Adicionar" />
     </div>
   );
 }
---
+++ add/EmphasisButton.tsx
@@ -0,0 +1,11 @@
+interface EmphasisButtonProps {
+  text: string;
+}
+
+export function EmphasisButton({ text }: EmphasisButtonProps) {
+  return (
+    <div className="flex items-center shadow-md justify-center bg-green-600 rounded-lg p-2 text-lg w-full text-white ">
+      {text}
+    </div>
+  );
+}
