94dde88c8f77d538660b137cd46c2f1b81d1de69
Ulisses <m.stunik@gmail.com>
2021-11-03 11:22:34 -0300

    turn TextInput compatible with input type="text"

--- add/AddItemScreen.tsx
+++ add/AddItemScreen.tsx
@@ -3,16 +3,29 @@ import { EmphasisButton } from "../forms/EmphasisButton";
 import { TextInput } from "../forms/TextInput";
 import { tags } from "./tags";
 import { TagsSelector } from "../forms/TagsSelector";
+import { useForm } from "react-hook-form";
 
 export function AddScreen() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  console.error(watch("name")); // watch input value by passing the name of it
+
   return (
-    <div className="flex flex-col flex-1 h-full items-stretch p-2">
-      <TextInput label="Nome" />
-      <AvailabilitySelector />
-      <TextInput label="Descrição" optional />
-      <TagsSelector label="Marcar como" options={tags} />
-      <TextInput label="Quantidade" optional />
-      <EmphasisButton text="Adicionar" />
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex flex-col flex-1 h-full items-stretch p-2">
+        <TextInput label="Nome" {...register("name")} />
+        <AvailabilitySelector />
+        <TextInput label="Descrição" optional />
+        <TagsSelector label="Marcar como" options={tags} />
+        <TextInput label="Quantidade" optional />
+        <EmphasisButton text="Adicionar" />
+      </div>
+    </form>
   );
 }
--- forms/TextInput.tsx
+++ forms/TextInput.tsx
@@ -1,31 +1,36 @@
+import { forwardRef, HTMLProps } from "react";
 import { Label } from "./Label";
 
-interface TextInputProps {
+interface TextInputProps extends HTMLProps<HTMLButtonElement> {
   label: string;
   optional?: boolean;
   leftChild?: JSX.Element;
 }
 
-export function TextInput({ label, optional, leftChild }: TextInputProps) {
-  return (
-    <label>
-      <div className="flex flex-col items-start transform -translate-y-1 mb-3">
-        <div className="relative transform translate-y-1 ">
-          <Label text={label} className="bg-white pb-1" />
+export const TextInput = forwardRef(
+  ({ label, optional, leftChild, ...rest }: TextInputProps, ref) => {
+    return (
+      <label>
+        <div className="flex flex-col items-start transform -translate-y-1 mb-3">
+          <div className="relative transform translate-y-1 ">
+            <Label text={label} className="bg-white pb-1" />
+          </div>
+          <div
+            className={
+              "flex flex-1 self-stretch flex-row border-2 border-gray-300 rounded-lg p-3 focus:border-black focus-within:border-gray-800"
+            }
+          >
+            {leftChild}
+            <input
+              ref={ref}
+              type="textarea"
+              placeholder={optional ? "opcional" : undefined}
+              className=" self-stretch flex flex-1 outline-none"
+              {...rest}
+            />
+          </div>
         </div>
-        <div
-          className={
-            "flex flex-1 self-stretch flex-row border-2 border-gray-300 rounded-lg p-3 focus:border-black focus-within:border-gray-800"
-          }
-        >
-          {leftChild}
-          <input
-            type="textarea"
-            placeholder={optional ? "opcional" : undefined}
-            className=" self-stretch flex flex-1 outline-none"
-          />
-        </div>
-      </div>
-    </label>
-  );
-}
+      </label>
+    );
+  }
+);
