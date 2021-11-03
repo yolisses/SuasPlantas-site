4173ae9af73ccd04daf0b0d0b6d61fe18bb2f6c5
Ulisses <m.stunik@gmail.com>
2021-11-03 08:12:55 -0300

    change gap

--- add/AvailabilitySelector.tsx
+++ add/AvailabilitySelector.tsx
@@ -5,7 +5,7 @@ export function AvailabilitySelector() {
   return (
     <div>
       <Label text="Disponível para" />
-      <div className="flex flex-row gap-3 mb-3 pt-1">
+      <div className="flex flex-row gap-2 mb-3 pt-1">
         <ToggleButton text="Doação" />
         <ToggleButton text="Troca" />
         <ToggleButton text="Venda" />
