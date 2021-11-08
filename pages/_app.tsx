d0a24bba786dca9e2120ad81e1b187cecbe6d7da
Ulisses <m.stunik@gmail.com>
2021-11-08 11:26:46 -0300

    separate icons tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,39 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { IconTags } from "./IconTags";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Plantes</title>
-
-        {/* icon */}
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/icon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/icon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/icon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/icon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/icon/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <meta name="msapplication-TileColor" content="#00a300" />
-        <meta name="theme-color" content="#ffffff" />
+        <IconTags />
       </Head>
       <Component {...pageProps} />
     </>
