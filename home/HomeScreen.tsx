25fe63e721a365a563888a64aef0bcc7000a2cc5
Ulisses <m.stunik@gmail.com>
2021-11-08 20:03:19 -0300

    fix missing ref

--- home/HomeScreen.tsx
+++ home/HomeScreen.tsx
@@ -1,6 +1,4 @@
-import { auth } from "../auth/auth";
 import { AddButton } from "./AddButton";
-import { ListItem } from "./ListItem";
 
 export function HomeScreen({ data }) {
   return (
@@ -9,7 +7,6 @@ export function HomeScreen({ data }) {
         <ListItem key={index} {...item} />
       ))} */}
       {JSON.stringify(data)}
-      {JSON.stringify(auth.user)}
       <div className=" z-20 fixed bottom-6 right-6">
         <AddButton />
       </div>
