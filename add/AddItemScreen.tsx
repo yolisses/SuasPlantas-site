d731495c134c49d440754b312826425aeea4b167
Ulisses <m.stunik@gmail.com>
2021-11-11 06:25:36 -0300

    make form responsive

--- add/AddItemScreen.tsx
+++ add/AddItemScreen.tsx
@@ -23,66 +23,68 @@ export function AddScreen() {
     <div>
       <Header />
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-col flex-1 h-full items-stretch p-2">
-          <TextInput
-            label="Nome"
-            {...register("name", { required })}
-            error={errors?.name?.message}
-            maxLength={128}
-          />
-          <FieldBox label="Disponível para" labelActive={false}>
-            <div className="self-stretch ">
-              <div className="flex flex-row gap-2 pt-1 ">
-                <ToggleButton
-                  text="Doação"
-                  className="flex flex-1"
-                  {...register("donate")}
-                />
-                <ToggleButton
-                  text="Troca"
-                  className="flex flex-1"
-                  {...register("swap")}
-                />
-                <ToggleButton
-                  text="Venda"
-                  className="flex flex-1"
-                  {...register("sell")}
-                />
+        <div className="flex flex-col items-center">
+          <div className="flex flex-col flex-1 h-full items-stretch p-2 max-w-md">
+            <TextInput
+              label="Nome"
+              {...register("name", { required })}
+              error={errors?.name?.message}
+              maxLength={128}
+            />
+            <FieldBox label="Disponível para" labelActive={false}>
+              <div className="self-stretch ">
+                <div className="flex flex-row gap-2 pt-1 ">
+                  <ToggleButton
+                    text="Doação"
+                    className="flex flex-1"
+                    {...register("donate")}
+                  />
+                  <ToggleButton
+                    text="Troca"
+                    className="flex flex-1"
+                    {...register("swap")}
+                  />
+                  <ToggleButton
+                    text="Venda"
+                    className="flex flex-1"
+                    {...register("sell")}
+                  />
+                </div>
               </div>
-            </div>
-          </FieldBox>
-          <TextInput
-            label="Preço"
-            type="number"
-            mask={allowNumbersRegex}
-            min={0}
-            max={9999}
-            maxLength={4}
-            leftChild={<MoneySign />}
-          />
-          <TextInput
-            optional
-            maxLength={2048}
-            label="Descrição"
-            {...register("description")}
-          />
-          <TagsSelector
-            label="Marcar como"
-            id="tags"
-            options={tags}
-            register={register}
-          />
-          <TextInput
-            label="Quantidade"
-            optional
-            {...register("amount")}
-            type="number"
-            min={1}
-            max={9999}
-            maxLength={4}
-            mask={allowNumbersRegex}
-          />
-          <EmphasisButton text="Adicionar" submit />
+            </FieldBox>
+            <TextInput
+              label="Preço"
+              type="number"
+              mask={allowNumbersRegex}
+              min={0}
+              max={9999}
+              maxLength={4}
+              leftChild={<MoneySign />}
+            />
+            <TextInput
+              optional
+              maxLength={2048}
+              label="Descrição"
+              {...register("description")}
+            />
+            <TagsSelector
+              label="Marcar como"
+              id="tags"
+              options={tags}
+              register={register}
+            />
+            <TextInput
+              label="Quantidade"
+              optional
+              {...register("amount")}
+              type="number"
+              min={1}
+              max={9999}
+              maxLength={4}
+              mask={allowNumbersRegex}
+            />
+            <EmphasisButton text="Adicionar" submit />
+          </div>
         </div>
       </form>
     </div>
