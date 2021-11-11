8f04c9ce5ee26d8bc1193791bab4a250ac114ef6
Ulisses <m.stunik@gmail.com>
2021-11-11 06:11:17 -0300

    remove "massa" from login screen

--- auth/SignInScreen.tsx
+++ auth/SignInScreen.tsx
@@ -1,30 +1,29 @@
-import GoogleLogin, {GoogleLoginResponse} from "react-google-login";
-import {observer} from "mobx-react";
-import {api} from "../api/api";
-import {useAuth} from "./AuthContext";
-import {Header} from "../common/Header";
+import GoogleLogin, { GoogleLoginResponse } from "react-google-login";
+import { observer } from "mobx-react";
+import { api } from "../api/api";
+import { useAuth } from "./AuthContext";
+import { Header } from "../common/Header";
 
 export const SignInScreen = observer(() => {
-    const {user, setUser} = useAuth();
+  const { user, setUser } = useAuth();
 
-    async function onSuccess(response: GoogleLoginResponse) {
-        const {access_token} = response.getAuthResponse(true);
-        const res = await api.post("google-sign-in", {access_token});
-        console.log(res)
-        setUser!(res.data.user);
-    }
+  async function onSuccess(response: GoogleLoginResponse) {
+    const { access_token } = response.getAuthResponse(true);
+    const res = await api.post("google-sign-in", { access_token });
+    console.log(res);
+    setUser!(res.data.user);
+  }
 
-    return (
-        <div>
-            <Header/>
-            <div>Use uma conta para prosseguir</div>
-            {JSON.stringify(user)}
-            <div onClick={onSuccess}>massa</div>
-            <GoogleLogin
-                clientId={process.env.GOOGLE_CLIENT_ID as string}
-                buttonText={"Continuar com o Google"}
-                onSuccess={onSuccess}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <div>Use uma conta para prosseguir</div>
+      {JSON.stringify(user)}
+      <GoogleLogin
+        clientId={process.env.GOOGLE_CLIENT_ID as string}
+        buttonText={"Continuar com o Google"}
+        onSuccess={onSuccess}
+      />
+    </div>
+  );
 });
