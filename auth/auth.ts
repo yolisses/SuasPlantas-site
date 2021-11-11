051b25c5e1fa449dcddf9c3f78c1a3f36bbc79a8
Ulisses <m.stunik@gmail.com>
2021-11-11 11:14:11 -0300

    persist user

--- auth/auth.ts
+++ auth/auth.ts
@@ -1,11 +1,18 @@
 import { makeAutoObservable } from "mobx";
 import { User } from "../types/User";
+import { makePersistable } from "mobx-persist-store";
 
 export class Auth {
   user: User | null = null;
 
   constructor() {
     makeAutoObservable(this);
+    if (process.browser)
+      makePersistable(this, {
+        name: "authStore",
+        properties: ["user"],
+        storage: window.localStorage,
+      });
   }
 }
 
