c5135a50db0d66b1fdcde4a0789c0c3594b76808
Ulisses <m.stunik@gmail.com>
2021-11-12 14:59:52 -0300

    add link on list item

--- home/ListItem.tsx
+++ home/ListItem.tsx
@@ -4,6 +4,7 @@ const link =
 import Image from "next/image";
 import { someImage } from "../mock/someImage";
 import { Plant } from "../types/Plant";
+import Link from "next/link";
 
 interface ListItemDTO {
   item: Plant;
@@ -12,19 +13,21 @@ interface ListItemDTO {
 export function ListItem({ item }: ListItemDTO) {
   const { name, description } = item;
   return (
-    <div className="flex flex-row h-32 mb-1 mx-1">
-      <div className="w-32 h-32 mr-1">
-        <Image
-          src={someImage}
-          width={200}
-          height={200}
-          className="bg-gray-200 flex-shrink-0 bg-cover rounded-xl"
-        />
+    <Link href={"plants/" + item.id}>
+      <div className="flex flex-row h-32 mb-1 mx-1">
+        <div className="w-32 h-32 mr-1">
+          <Image
+            src={someImage}
+            width={200}
+            height={200}
+            className="bg-gray-200 flex-shrink-0 bg-cover rounded-xl"
+          />
+        </div>
+        <div className="flex-1 overflow-hidden">
+          <div>{name}</div>
+          <div className="text-gray-600 text-sm">{description}</div>
+        </div>
       </div>
-      <div className="flex-1 overflow-hidden">
-        <div>{name}</div>
-        <div className="text-gray-600 text-sm">{description}</div>
-      </div>
-    </div>
+    </Link>
   );
 }
--- types/Plant.ts
+++ types/Plant.ts
@@ -1,5 +1,8 @@
 export interface Plant {
+  id: PlantId;
   name: string;
   description: string;
   card: string;
 }
+
+export type PlantId = number;
