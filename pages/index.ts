d2ec4ae84a1d42bb17c8d06faf0bfb0ee49a3ff9
Ulisses <m.stunik@gmail.com>
2021-11-12 12:08:25 -0300

    add revalidate of 1 second to home screen

--- pages/index.ts
+++ pages/index.ts
@@ -1,12 +1,14 @@
+import { GetStaticProps } from "next";
 import { api } from "../api/api";
 import { HomeScreen } from "../home/HomeScreen";
 
 export default HomeScreen;
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const res = await api.get("plants");
 
   return {
-    props: { data: res.data }, // will be passed to the page component as props
+    props: { data: res.data },
+    revalidate: 1,
   };
-}
+};
