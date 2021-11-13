531fca104e1a36fe53c2b1c0a81d6b4076c6ac76
Ulisses <m.stunik@gmail.com>
2021-11-13 07:03:55 -0300

    remove edit on search top

--- common/SearchTop.tsx
+++ common/SearchTop.tsx
@@ -5,12 +5,9 @@ export function SearchTop() {
   return (
     <Link href="/search">
       <div className="bg-white flex-1 text-black h-9 rounded-full flex flex-row items-center p-3">
-        <input
-          type="text"
-          contentEditable={false}
-          placeholder="Pesquisar"
-          className="flex-1 focus:border-transparent outline-none bg-transparent w-full"
-        />
+        <div className="flex-1 focus:border-transparent outline-none bg-transparent w-full text-gray-400">
+          Pesquisar
+        </div>
         <FaSearch className="ml-2" />
       </div>
     </Link>
