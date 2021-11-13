57721d0f5d80fe5e1700f9d30a45687d561bd9a8
Ulisses <m.stunik@gmail.com>
2021-11-13 07:13:06 -0300

    change title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <DataContextProvider>
       <div className="flex flex-col flex-1 flex-wrap overflow-x-hidden">
         <Head>
-          <title>Plantes - Troque mudas de plantas online</title>
+          <title>Plantes - Trocar mudas de plantas</title>
           <IconTags />
         </Head>
         <Component {...pageProps} />
