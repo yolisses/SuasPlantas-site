0e007138de4a114d2afa2dcef4f9886f786f3f6b
Ulisses <m.stunik@gmail.com>
2021-11-15 08:48:36 -0300

    add google search console tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     <DataContextProvider>
       <div className="flex flex-col flex-1 flex-wrap overflow-x-hidden">
         <Head>
+          <meta
+            name="google-site-verification"
+            content="XipRkG04zmk3gcBYI2q_HzJSU2F6BaT6jbz5N57ilZ8"
+          />
           <title>Plantes - Trocar mudas de plantas</title>
           <IconTags />
         </Head>
