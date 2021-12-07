737a324e440c31eb3b7dcf22adc1c9e2982d8f2b
Ulisses <m.stunik@gmail.com>
2021-12-07 16:30:41 -0300

    make the button data accessable

--- auth/GoogleButton.tsx
+++ auth/GoogleButton.tsx
@@ -1,23 +1,40 @@
-export function GoogleButton() {
-  return (
-    <div>
-      <div
-        id="g_id_onload"
-        data-ux_mode="popup"
-        data-context="signin"
-        data-auto_prompt="false"
-        data-client_id={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
-      />
-      <div
-        data-shape="pill"
-        data-size="large"
-        data-text="signin"
-        data-type="standard"
-        data-callback="mozao"
-        className="g_id_signin"
-        data-theme="filled_blue"
-        data-logo_alignment="left"
-      />
-    </div>
-  );
+import { Component } from "react";
+
+export class GoogleButton extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  customCallback(e) {
+    console.log("ONE TAP version 2 ", e);
+  }
+
+  componentDidMount() {
+    window.customCallback = this.customCallback;
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          id="g_id_onload"
+          data-auto_prompt="false"
+          data-client_id={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
+          data-callback="customCallback"
+        />
+
+        <div
+          data-width="300"
+          data-shape="pill"
+          data-size="large"
+          data-text="signin"
+          data-type="standard"
+          data-callback="mozao"
+          className="g_id_signin"
+          data-theme="filled_blue"
+          data-logo_alignment="left"
+        />
+      </div>
+    );
+  }
 }
