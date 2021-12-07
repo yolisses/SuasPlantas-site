f373c9cc6c3379fb3d9fa4b47d9ab674595d5c7e
Ulisses <m.stunik@gmail.com>
2021-12-07 11:55:56 -0300

    add error logs

--- api/apollo-client.ts
+++ api/apollo-client.ts
@@ -1,9 +1,29 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_API_URL,
   cache: new InMemoryCache(),
   credentials: "include",
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (networkError) {
+        console.error(`[Network error]: ${networkError}`);
+      }
+      if (graphQLErrors) {
+        graphQLErrors.forEach(({ message, locations, path }) =>
+          console.error(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      }
+    }),
+    new HttpLink({ uri: process.env.NEXT_PUBLIC_API_URL }),
+  ]),
 });
 
 export default client;
