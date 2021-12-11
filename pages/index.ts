a652f4e3416522f52f8160ccd381772a89a1a45a
Ulisses <m.stunik@gmail.com>
2021-12-11 11:12:02 -0300

    use new api on index

--- pages/index.ts
+++ pages/index.ts
@@ -1,30 +1,14 @@
-import gql from "graphql-tag";
 import { GetStaticProps } from "next";
-import client from "../api/apollo-client";
+import { api } from "../api/api";
 import { HomeScreen } from "../home/HomeScreen";
 
 export default HomeScreen;
 
 export const getStaticProps: GetStaticProps = async () => {
-  const query = gql`
-    query {
-      plants {
-        edges {
-          node {
-            id
-            name
-            description
-            price
-            swap
-            donate
-          }
-        }
-      }
-    }
-  `;
-  const res = await client.query({ query, fetchPolicy: "no-cache" });
+  const res = await api.get('plants')
+  console.log(res.data)
   return {
-    props: { data: res.data.plants.edges.map((e: any) => e.node) },
+    props: { data: res.data.content },
     revalidate: 1,
   };
 };
