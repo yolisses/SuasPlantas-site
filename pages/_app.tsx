569e10455d0aa547e52c3cda396a1c85a1f0990b
Ulisses <m.stunik@gmail.com>
2021-12-15 10:05:56 -0300

    add google analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,27 @@ import { observer } from "mobx-react-lite";
 import { FaviconTags } from "../common/FaviconTags";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const googleAnalyticsScript = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-BCW0VD90L1');`;
+
   return (
     <DataContextProvider>
       <>
         <Head>
+          {/* google login */}
           <meta
             name="google-site-verification"
             content="XipRkG04zmk3gcBYI2q_HzJSU2F6BaT6jbz5N57ilZ8"
           />
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-BCW0VD90L1"
+          />
+          <script>{googleAnalyticsScript}</script>
           <title>SuasPlantas - Trocar mudas de plantas</title>
           <FaviconTags />
         </Head>
