520c7823e5ae4633e0ded0bdf4bffbffa62ca3ed
Ulisses <m.stunik@gmail.com>
2021-12-19 14:31:33 -0300

    add axios error interceptor

--- api/api.ts
+++ api/api.ts
@@ -3,3 +3,15 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL + "/",
 });
+
+api.interceptors.response.use(undefined, (error) => {
+  console.error("macaxeira");
+  console.error(Object.keys(error));
+  console.error(Object.keys(error.response));
+  throw new Error(
+    JSON.stringify({
+      status: error.response.status,
+      message: error.response,
+    })
+  );
+});
