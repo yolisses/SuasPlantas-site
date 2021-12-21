3acdc0b4bc58a0049557c41bd4b4c74048ca080f
Ulisses <m.stunik@gmail.com>
2021-12-21 12:05:46 -0300

    get plants on homescreen by get server side props

--- pages/index.ts
+++ pages/index.ts
@@ -1,12 +1,12 @@
-import { GetStaticProps } from 'next';
+import { GetServerSideProps } from 'next';
 import { api } from '../api/api';
 import { HomeScreen } from '../home/HomeScreen';
 
 export default HomeScreen;
 
-export const getStaticProps: GetStaticProps = async () =>
-  // const res = await api.get('plants');
-  ({
-    props: { data: [] },
-    revalidate: 1,
-  });
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await api.get('plants');
+  return {
+    props: { data: res.data.content },
+  };
+};
