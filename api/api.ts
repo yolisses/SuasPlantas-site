3332bc04af9201ccde540b9dd72a408d690c8ee6
Ulisses <m.stunik@gmail.com>
2021-12-23 13:12:56 -0300

    add alternative error message source

--- api/api.ts
+++ api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { isDev } from '../utils/isDev';
 
-const useDevApi = false;
+const useDevApi = true;
 const baseUrl = isDev && useDevApi
   ? process.env.NEXT_PUBLIC_DEV_API_URL
   : process.env.NEXT_PUBLIC_API_URL;
@@ -18,7 +18,7 @@ api.interceptors.response.use(undefined, (error) => {
   throw new Error(
     JSON.stringify({
       status: error?.response?.status,
-      message: error?.response?.data || error?.reason,
+      message: error?.response?.data || error?.reason || error?.message,
     }),
   );
 });
