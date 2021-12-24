b27001b7659891b2a43ce5f5275eaae4268afb78
Ulisses <m.stunik@gmail.com>
2021-12-24 15:00:17 -0300

    remove headers from userscreen server side props

--- pages/users/[id].ts
+++ pages/users/[id].ts
@@ -1,13 +1,11 @@
-import { AxiosRequestHeaders } from 'axios';
 import { GetServerSideProps } from 'next';
 import { api } from '../../api/api';
 import { UserScreen } from '../../user/UserScreen';
 
 export default UserScreen;
-export const getServerSideProps: GetServerSideProps = async ({ params, req }) => {
-  const res = await api.get(`users/${params!.id}`, {
-    headers: { ...req.headers } as AxiosRequestHeaders,
-  });
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const res = await api.get(`users/${params!.id}`);
   return {
     props: { user: res.data },
   };
