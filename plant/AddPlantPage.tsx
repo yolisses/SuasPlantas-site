9d514768f7e4c6fb8488f3464d36e98e0e97a181
Ulisses <m.stunik@gmail.com>
2021-12-26 10:11:16 -0300

    add registers to add plant screen

--- plant/AddPlantPage.tsx
+++ plant/AddPlantPage.tsx
@@ -1,44 +1,56 @@
 import {
-  Checkbox, FormControlLabel, FormGroup, FormLabel, InputAdornment, TextField, Toolbar,
+  Checkbox, FormControlLabel, FormGroup, FormLabel, InputAdornment, TextField,
 } from '@mui/material';
 import Button from '@mui/material/Button';
 import Autocomplete from '@mui/material/Autocomplete';
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import { Controller, useForm } from 'react-hook-form';
+import { useState } from 'react';
 import { ImagesInput } from '../forms/ImagesInput';
 import { Header } from '../common/Header';
 import { tags } from './tags';
 
 export function AddPlantPage() {
-  const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
-  const checkedIcon = <CheckBoxIcon fontSize="small" />;
+  const [sell, setSell] = useState(false);
+  const { register, handleSubmit, control } = useForm();
+
+  function submit(data) {
+    console.log(data);
+  }
 
   return (
     <>
       <Header />
       <div className="w-full flex-1 flex flex-col justify-center items-center">
         <div className="flex flex-col p-2 gap-4 max-w-lg w-full pb-8">
-          <ImagesInput />
-          <TextField label="Nome" />
-          <TextField label="Descrição" multiline minRows={2} />
-          <Autocomplete
-            multiple
-            options={tags}
-            disableCloseOnSelect
-            getOptionLabel={(option) => option}
-            renderOption={(props, option, { selected }) => (
-              <li {...props}>
-                <Checkbox
-                  icon={icon}
-                  checkedIcon={checkedIcon}
-                  style={{ marginRight: 8 }}
-                  checked={selected}
-                />
-                {option}
-              </li>
-            )}
-            renderInput={(params) => (
-              <TextField {...params} label="Marcar como" />
+          <Controller
+            name="images"
+            control={control}
+            render={
+            ({ field: { onChange } }) => <ImagesInput onChange={onChange} />
+          }
+          />
+          <TextField label="Nome" {...register('name')} />
+          <TextField label="Descrição" multiline minRows={2} {...register('description')} />
+          <Controller
+            name="tags"
+            control={control}
+            render={({ field: { onChange } }) => (
+              <Autocomplete
+                multiple
+                options={tags}
+                disableCloseOnSelect
+                onChange={(_, value) => onChange(value)}
+                renderOption={(props, option, { selected }) => (
+                  <li {...props}>
+                    <Checkbox
+                      style={{ marginRight: 8 }}
+                      checked={selected}
+                    />
+                    {option}
+                  </li>
+                )}
+                renderInput={(params) => (<TextField label="Marcar como" {...params} />)}
+              />
             )}
           />
           <TextField
@@ -47,34 +59,39 @@ export function AddPlantPage() {
             InputProps={{
               inputProps: { min: 1, max: 100, pattern: '[0-9]*' },
             }}
+            {...register('amount')}
           />
           <div>
             <FormLabel component="legend">Disponível para</FormLabel>
             <FormGroup className="flex-row justify-between">
               <FormControlLabel
-                control={<Checkbox />}
                 label="Doação"
+                control={(<Checkbox {...register('donate')} />)}
               />
               <FormControlLabel
-                control={<Checkbox />}
                 label="Troca"
+                control={(<Checkbox {...register('swap')} />)}
               />
               <FormControlLabel
-                control={<Checkbox />}
                 label="Venda"
+                control={(<Checkbox onChange={(e, checked) => setSell(checked)} />)}
               />
             </FormGroup>
+            {sell
+            && (
             <TextField
               label="Preço"
               type="number"
               className="w-full mt-2"
+              {...register('price')}
               InputProps={{
                 inputProps: { min: 0, max: 100, pattern: '[0-9]*' },
                 startAdornment: <InputAdornment position="start">R$</InputAdornment>,
               }}
             />
+            )}
           </div>
-          <Button variant="contained" className="h-12">
+          <Button variant="contained" className="h-12" onClick={handleSubmit(submit)}>
             {/* <CircularProgress size={20} className="mr-2" /> */}
             Adicionar
           </Button>
