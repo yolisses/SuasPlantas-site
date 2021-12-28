1edfb1ba55f48f86504fa49ac4b949bd94ef5791
Ulisses <m.stunik@gmail.com>
2021-12-28 11:36:04 -0300

    try to fix analytics tag

--- config/GoogleAnalyticsTags.tsx
+++ config/GoogleAnalyticsTags.tsx
@@ -1,14 +1,16 @@
 export function GoogleAnalyticsTags() {
+  const config = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-215426535-1');`;
+
   return (
     <>
       <script async src="https://www.googletagmanager.com/gtag/js?id=UA-215426535-1" />
       <script async defer>
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-215426535-1');`}
+        {config}
       </script>
     </>
   );
