9ec799134cbe72a3261b6e6821b6c9a7d41cd015
Ulisses <m.stunik@gmail.com>
2021-12-28 11:44:08 -0300

    try to fix goohle analytics

--- config/GoogleAnalyticsTags.tsx
+++ config/GoogleAnalyticsTags.tsx
@@ -1,16 +1,15 @@
 export function GoogleAnalyticsTags() {
-  const config = `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-215426535-1');`;
-
   return (
     <>
       <script async src="https://www.googletagmanager.com/gtag/js?id=UA-215426535-1" />
       <script async defer>
-        {config}
+        window.dataLayer = window.dataLayer || [];
+        function gtag()
+        {'{'}
+        dataLayer.push(arguments);
+        {'}'}
+        gtag('js', new Date());
+        gtag('config', 'UA-215426535-1');
       </script>
     </>
   );
