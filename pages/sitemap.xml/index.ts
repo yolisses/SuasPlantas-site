a417811611cea7c717c5f82d17d4b00a088ade6c
Ulisses <m.stunik@gmail.com>
2021-12-31 18:14:51 -0300

    add real plants to sitemap

--- pages/sitemap.xml/index.ts
+++ pages/sitemap.xml/index.ts
@@ -1,6 +1,8 @@
 import { GetServerSideProps } from 'next';
 import { SitemapStream, streamToPromise } from 'sitemap';
 import { Readable } from 'stream';
+import { api } from '../../api/api';
+import { Plant } from '../../types/Plant';
 
 export default function Sitemap() {}
 
@@ -8,6 +10,7 @@ interface SitemapUrl{
  url:string
  changefreq:'daily'|'monthly'
  priority:number
+ lastmod:string
 }
 
 function coisa(links:SitemapUrl[]) {
@@ -21,10 +24,14 @@ function coisa(links:SitemapUrl[]) {
 export const getServerSideProps:GetServerSideProps = async ({ res }) => {
   const coisas = [70, 71];
 
-  const links = coisas.map((id) :SitemapUrl => ({
-    url: `/plants/${id}`,
+  const response :any = await api.get('plants/sitemap');
+  console.log(response);
+
+  const links = response.data.map((plant:Plant) :SitemapUrl => ({
+    url: `/plants/${plant.id}`,
     changefreq: 'daily',
     priority: 0.9,
+    lastmod: plant.updatedAt,
   }));
   const result = await coisa(links);
 
