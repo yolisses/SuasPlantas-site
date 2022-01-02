ef70d0e3842e00e765356e33ee39e50f3b14b9e1
Ulisses <m.stunik@gmail.com>
2022-01-02 13:40:04 -0300

    make logout button work

--- user/SignOutButton.tsx
+++ user/SignOutButton.tsx
@@ -3,12 +3,6 @@ import { api } from '../api/api';
 import { authStore } from '../auth/authStore';
 
 export const SignOutButton = function () {
-  async function handleClick() {
-    authStore.user = undefined;
-    authStore.token = undefined;
-    return api.post('users/logout');
-  }
-
   return (
     <Button onClick={handleClick}>
       Sign out
