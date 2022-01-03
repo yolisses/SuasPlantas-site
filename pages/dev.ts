b4f8f8449f9f6f2c62785ef0a3f7bc3b30f78333
Ulisses <m.stunik@gmail.com>
2022-01-03 10:44:05 -0300

    make dev screen not found in prod

--- pages/dev.ts
+++ pages/dev.ts
@@ -1,3 +1,12 @@
+import { GetServerSideProps } from 'next';
 import { DevScreen } from '../dev/DevScreen';
+import { isDev } from '../utils/isDev';
 
 export default DevScreen;
+
+export const getServerSideProps : GetServerSideProps = async function () {
+  return {
+    props: {},
+    notFound: !isDev,
+  };
+};
