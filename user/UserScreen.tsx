7d31ddd9cbf94435037d142cc7e5010e88dbeaa7
Ulisses <m.stunik@gmail.com>
2022-01-05 19:50:57 -0300

    change use default header on user screen

--- user/UserScreen.tsx
+++ user/UserScreen.tsx
@@ -1,14 +1,13 @@
 import Image from 'next/image';
 import { Button, Link } from '@mui/material';
 import { FaRegUser } from 'react-icons/fa';
-import { HeaderLayout } from '../common/HeaderLayout';
-import { loremIpsum } from '../mock/loremIpsum';
 import { User } from './User';
 import { GridItem } from '../common/GridItem';
 import { authStore } from '../auth/authStore';
 import { userImage } from '../images/user';
 import { WhatsappButton } from '../contact/WhatsappButton';
 import { InstagramButton } from '../contact/InstagramButton';
+import { Header } from '../common/Header';
 
 interface UserScreenProps {
   user: User;
@@ -17,14 +16,16 @@ interface UserScreenProps {
 export function UserScreen({ user }: UserScreenProps) {
   return (
     <div className="flex flex-col items-center w-full">
-      <HeaderLayout className="shadow-sm">{user.name}</HeaderLayout>
+      {/* <HeaderLayout className="shadow-sm">{user.name}</HeaderLayout> */}
+      <Header />
       <div className="p-2 flex flex-col gap-4 max-w-4xl">
-        <div className="flex flex-row gap-2 items-center">
+        <div className="flex flex-row gap-2 items-center pt-4">
           <Image
-            className="rounded-full bg-cover w-24 h-24 object-cover"
-            src={user.image || userImage}
-            height={100}
             width={100}
+            height={100}
+            objectFit="cover"
+            src={user.image || userImage}
+            className="rounded-full bg-cover w-24 h-24"
           />
           <div className="flex-1 flex-wrap flex overflow-ellipsis">
             <div className="flex flex-col w-full">
