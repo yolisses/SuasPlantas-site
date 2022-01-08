29c855368f7ce028d586bcbb84571fe6ddc27ce0
Ulisses <m.stunik@gmail.com>
2022-01-08 16:14:48 -0300

    fix hasContact

--- utils/hasContact.ts
+++ utils/hasContact.ts
@@ -1,5 +1,5 @@
 import { User } from '../user/User';
 
 export function hasContact(user:User) {
-  return !!(user.whatsappNumber && user.instagramUsername);
+  return !!(user.whatsappNumber || user.instagramUsername);
 }
