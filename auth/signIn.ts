b4f3829525eb9c3df05c40f4a0bc47b22f2d999c
Ulisses <m.stunik@gmail.com>
2022-01-09 12:24:10 -0300

    remove  nookies from signin

--- auth/signIn.ts
+++ auth/signIn.ts
@@ -1,4 +1,3 @@
-import { setCookie } from 'nookies';
 import { api } from '../api/api';
 import { authStore } from './authStore';
 
@@ -7,8 +6,5 @@ export async function signIn(
   accessToken: string,
 ) {
   const res = await api.post('users', { provider, accessToken });
-  const token = res.headers.authorization;
   authStore.setUser(res.data);
-  setCookie(undefined, 'connect.sid', token);
-  return token;
 }
