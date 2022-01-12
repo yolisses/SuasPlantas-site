c18e64df7b7500aa6a6f9b6fb643fe804f102d10
Ulisses <m.stunik@gmail.com>
2022-01-12 15:27:24 -0300

    fix require login

--- auth/RequireLogin.tsx
+++ auth/RequireLogin.tsx
@@ -3,14 +3,12 @@ import { modalStore } from '../modal/modalStore';
 import { authStore } from './authStore';
 import { SignInBox } from './SignInBox';
 
-export function RequireLogin({ children, onClick, ...rest }:any) {
+export function RequireLogin({ children, ...rest }:any) {
   function handleClick(e:MouseEvent<HTMLDivElement, MouseEvent>) {
     if (!authStore.user) {
       modalStore.setModal(<SignInBox />);
       e.stopPropagation();
       e.preventDefault();
-    } else {
-      onClick(e);
     }
   }
 
