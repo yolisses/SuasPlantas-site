1269ae38040830e34a78548ebc366c30319383f6
Ulisses <m.stunik@gmail.com>
2022-01-14 18:46:42 -0300

    reconfigure next pwa

--- next.config.js
+++ next.config.js
@@ -1,36 +1,39 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
 const withPWA = require('next-pwa');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer(withPWA({
-  reactStrictMode: true,
-  images: {
-    domains: [
-      'unsplash.com',
-      'images.unsplash.com',
-      'github.com',
-      'lh3.googleusercontent.com',
-      'plantes.s3.sa-east-1.amazonaws.com',
-      'unsplash.it',
-      'platform-lookaside.fbsbx.com',
-      'suasplantas.s3.sa-east-1.amazonaws.com',
-      'plantei-dev.s3.sa-east-1.amazonaws.com',
-    ],
+module.exports = withPlugins(
+  [withPWA, withBundleAnalyzer],
+  {
+    reactStrictMode: true,
+    images: {
+      domains: [
+        'unsplash.com',
+        'images.unsplash.com',
+        'github.com',
+        'lh3.googleusercontent.com',
+        'plantes.s3.sa-east-1.amazonaws.com',
+        'unsplash.it',
+        'platform-lookaside.fbsbx.com',
+        'suasplantas.s3.sa-east-1.amazonaws.com',
+        'plantei-dev.s3.sa-east-1.amazonaws.com',
+      ],
+    },
+    async redirects() {
+      return [
+        {
+          source: '/plants',
+          destination: '/',
+          permanent: true,
+        },
+      ];
+    },
+    pwa: {
+      dest: 'public',
+      disable: process.env.NODE_ENV === 'development',
+    },
   },
-  async redirects() {
-    return [
-      {
-        source: '/plants',
-        destination: '/',
-        permanent: true,
-      },
-    ];
-  },
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  },
-}));
+);
