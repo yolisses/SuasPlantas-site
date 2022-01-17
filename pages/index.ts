a6d652b2d5006cfb290c2bd6488cc1b7d11d35a4
Ulisses <m.stunik@gmail.com>
2022-01-17 09:21:16 -0300

    remove server side props from home page

--- pages/index.ts
+++ pages/index.ts
@@ -3,18 +3,3 @@ import { api } from '../api/api';
 import { HomePage } from '../home/HomePage';
 
 export default HomePage;
-
-export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-  const props:any = {
-    query,
-  };
-  try {
-    const res = await api.get('plants');
-    props.data = res.data;
-  } catch (err) {
-    props.err = err;
-  }
-  return {
-    props,
-  };
-};
