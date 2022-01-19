15f0cb41b837c9a9bc0641bb1fe53ec3c8f346fc
Ulisses <m.stunik@gmail.com>
2022-01-19 15:50:30 -0300

    try to fix menu strange icon bug

--- user/MeButton.tsx
+++ user/MeButton.tsx
@@ -8,29 +8,34 @@ import { RequireLogin } from '../auth/RequireLogin';
 
 export const MeButton = observer(() => {
   const size = 30;
-  return authStore.user ? (
-    <Link href={`/users/${authStore.user?.id}`}>
-      <a>
-        <button
-          className="icon-button p-2"
-        >
-          <Image
-            width={size}
-            height={size}
-            objectFit="cover"
-            alt={authStore.user?.name}
-            src={authStore.user?.image || userImage}
-            className="bg-gray-300 rounded-full shadow-md cursor-pointer"
-          />
-        </button>
-      </a>
-    </Link>
-  ) : (
-    <RequireLogin>
-      <button className="flex flex-row items-center gap-1 cursor-pointer text-white">
-        <FaRegUser size={18} />
-        <div className="text-lg">Entrar</div>
-      </button>
-    </RequireLogin>
+  return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      { authStore.user ? (
+        <Link href={`/users/${authStore.user?.id}`}>
+          <a>
+            <button
+              className="icon-button p-2"
+            >
+              <Image
+                width={size}
+                height={size}
+                objectFit="cover"
+                alt={authStore.user?.name}
+                src={authStore.user?.image || userImage}
+                className="bg-gray-300 rounded-full shadow-md cursor-pointer"
+              />
+            </button>
+          </a>
+        </Link>
+      ) : (
+        <RequireLogin>
+          <button className="flex flex-row items-center gap-1 cursor-pointer text-white">
+            <FaRegUser size={18} />
+            <div className="text-lg">Entrar</div>
+          </button>
+        </RequireLogin>
+      )}
+    </>
   );
 });
