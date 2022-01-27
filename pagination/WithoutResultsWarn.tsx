3c696a125c501660895d832ffbe0ec8e25fa3eae
Ulisses <m.stunik@gmail.com>
2022-01-27 11:32:46 -0300

    fix without results warn

--- pagination/WithoutResultsWarn.tsx
+++ pagination/WithoutResultsWarn.tsx
@@ -1,8 +1,11 @@
 import { usePlants } from '../plant/plantsContext';
+import { useQuests } from '../quest/questsContext';
+import { useTextSearchContext } from '../search/TextSearchContext';
 
 export function WithoutResultsWarn() {
-  const { filters, setFilters } = usePlants();
-  const text = filters?.text;
+  const { setFilters: setPlantsFilters } = usePlants();
+  const { setFilters: setQuestsFilters } = useQuests();
+  const { text, setText } = useTextSearchContext();
 
   return (
     <div className="flex flex-col items-center max-w-sm">
@@ -15,7 +18,14 @@ export function WithoutResultsWarn() {
           ? 'Verifique a ortografia, altere os filtros ou tente usar um termo de pesquisa menos específico.'
           : 'Você pode tentar mudar os filtros de pesquisa.'}
       </div>
-      <button onClick={() => { setFilters({}); }}>Página inicial</button>
+      <button onClick={() => {
+        setPlantsFilters(undefined);
+        setQuestsFilters(undefined);
+        setText(undefined);
+      }}
+      >
+        Página inicial
+      </button>
     </div>
   );
 }
