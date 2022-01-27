5d9a92094e16aa6e8178ee5f06916fc058dca7e0
Ulisses <m.stunik@gmail.com>
2022-01-27 12:03:15 -0300

    lint

--- common/menu/Menu.tsx
+++ common/menu/Menu.tsx
@@ -1,12 +1,19 @@
-import { ReactNode, useEffect } from 'react';
+import {
+  ReactNode,
+  useEffect,
+} from 'react';
 
 interface MenuProps{
-  children:ReactNode
   open:boolean
   onClose:()=>void
+  children:ReactNode
 }
 
-export function Menu({ children, open, onClose }:MenuProps) {
+export function Menu({
+  open,
+  onClose,
+  children,
+}:MenuProps) {
   useEffect(() => {
     window.addEventListener('click', onClose);
     return () => window.removeEventListener('click', onClose);
@@ -16,7 +23,10 @@ export function Menu({ children, open, onClose }:MenuProps) {
     return (
       <div className="relative">
         <div className="absolute right-0 w-screen sm:w-auto max-w-sm px-2 pb-12">
-          <div className="bg-white p-2 rounded-lg shadow-md overflow-y-auto" style={{ maxHeight: 'calc(100vh - 3.5rem)' }}>
+          <div
+            style={{ maxHeight: 'calc(100vh - 3.5rem)' }}
+            className="bg-white p-2 rounded-lg shadow-md overflow-y-auto"
+          >
             {children}
           </div>
         </div>
