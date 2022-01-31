b17718a1c30b356dfbc5f8f7eccc597d60ab3115
Ulisses <m.stunik@gmail.com>
2022-01-31 12:29:46 -0300

    don't show self user on users

--- user/UsersPage.tsx
+++ user/UsersPage.tsx
@@ -2,8 +2,11 @@ import { HomePage } from '../home/HomePage';
 import { usersContext } from './usersContext';
 import { UserItem } from './UserItem';
 import { User } from './User';
+import { useUser } from '../auth/userContext';
 
 export function UsersPage() {
+  const { user } = useUser();
+
   return (
     <HomePage
       context={usersContext}
@@ -11,7 +14,7 @@ export function UsersPage() {
     >
       {(items) => (
         <div className="p-2 pt-4 grid gap-2 grid-cols-2 md:grid-cols-5 xl:px-20">
-          {!!items && items.map((item: User) => (
+          {!!items && items.filter((item:User) => item.id !== user.id).map((item: User) => (
             <UserItem key={item.id} item={item} />
           ))}
         </div>
