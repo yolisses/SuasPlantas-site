ef761fe247dc403bb4c84077a75c33c8e2ab6c0b
Ulisses <m.stunik@gmail.com>
2022-01-31 15:39:05 -0300

    add tour blocks

--- home/PlantsPage.tsx
+++ home/PlantsPage.tsx
@@ -1,16 +1,20 @@
 import { HomePage } from './HomePage';
 import { Plant } from '../plant/Plant';
 import { GridItem } from '../common/GridItem';
+import { useTour } from '../tour/TourContext';
 import { homeTourSteps } from '../tour/homeTourSteps';
 import { plantsContext } from '../plant/plantsContext';
 
 export function PlantsPage() {
+  const { blockHomeTour } = useTour();
+
   return (
     <HomePage
       tab="plants"
+      tourName="Home"
       fabPath="plants/add"
       context={plantsContext}
-      tourSteps={homeTourSteps}
+      tourSteps={!blockHomeTour ? homeTourSteps : undefined}
     >
       {(items) => (
         <div className="p-2 pt-4 grid gap-2 grid-cols-2 md:grid-cols-5 xl:px-20">
