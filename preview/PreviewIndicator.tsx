c753d3ff0e73d4ccfb351535cf697d3b89528be3
Ulisses <m.stunik@gmail.com>
2022-02-04 18:53:48 -0300

    styling

--- preview/PreviewIndicator.tsx
+++ preview/PreviewIndicator.tsx
@@ -1,7 +1,7 @@
 export function PreviewIndicator() {
   return (
     <span
-      className="text-white bg-indigo-400 p-1 rounded-md text-center font-normal"
+      className="text-white bg-indigo-500 p-1 rounded-md text-center font-normal"
       style={{ fontSize: '0.8rem' }}
     >
       prévia
