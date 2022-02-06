2e8965e2dc66eee2470ed280c43ab17fa48ba31c
Ulisses <m.stunik@gmail.com>
2022-02-06 17:36:15 -0300

    fix build

--- dev/DevPage.tsx
+++ dev/DevPage.tsx
@@ -1,35 +1,7 @@
-import { ImageInputCustomRef, ImagesInput } from '../images/ImagesInput';
-import { getFileFromImageUrl } from '../upload/getFileFromImageUrl';
-import { Sending } from '../upload/Sending';
-
 export function DevPage() {
-  const customRef: ImageInputCustomRef = { };
-
-  const fakeImage = 'https://upload.wikimedia.org/wikipedia/commons/7/72/Coisa_Nossa%2C_Santana_do_Livramento.jpg';
-
   return (
     <div>
-      <button onClick={async () => {
-        if (customRef.current) {
-          const file = await getFileFromImageUrl(fakeImage);
-          customRef.current.addSending(
-            new Sending(
-              {
-                file,
-                onUpdate: () => { console.log('update'); },
-              },
-            ),
-          );
-        }
-      }}
-      >
-        mágica
-      </button>
-      <ImagesInput
-        customRef={customRef}
-        onBlur={(e) => console.log(e)}
-        onChange={(e) => console.log(e)}
-      />
+      oie
     </div>
   );
 }
--- images/SelectedImageItem.tsx
+++ images/SelectedImageItem.tsx
@@ -1,9 +1,13 @@
 import { FaTimes } from 'react-icons/fa';
 
-export function SelectedImageItem({ handleRemoveClick, src, hideCloseButton }) {
+interface SelectedImageItemProps{
+  handleRemoveClick:()=>void
+  src?:string
+}
+
+export function SelectedImageItem({ handleRemoveClick, src }:SelectedImageItemProps) {
   return (
     <div>
-      {!hideCloseButton && (
       <div className="relative z-10">
         <FaTimes
           size={26}
@@ -12,7 +16,6 @@ export function SelectedImageItem({ handleRemoveClick, src, hideCloseButton }) {
           className="bg-white absolute right-1 top-1 rounded-full p-1 shadow-lg cursor-pointer hover:bg-gray-100"
         />
       </div>
-      )}
       <div>
         <img
           alt=""
