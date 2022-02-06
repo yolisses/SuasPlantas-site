80b928a40bf43b4ff019c8f943139a607d49ad37
Ulisses <m.stunik@gmail.com>
2022-02-06 14:17:18 -0300

    refatorate

--- plant/ImagesSuggestions.tsx
+++ plant/ImagesSuggestions.tsx
@@ -10,13 +10,13 @@ export function ImagesSuggestions({ text }:{text:string}) {
   const [loading, setLoading] = useState(false);
   const [images, setImages] = useState<ImageSuggestion[]>();
 
-  const badResults = images && (images.length < 4);
-
   async function refresh() {
     setLoading(true);
     try {
-      const res = await api.get('images/suggest', { params: { text: `planta ${text}` } });
-      setImages(res.data);
+      const { data } = await api.get('images/suggest', {
+        params: { text: `planta ${text}` },
+      });
+      setImages(data);
     } catch (err:any) {
       console.error(err);
     }
@@ -39,26 +39,24 @@ export function ImagesSuggestions({ text }:{text:string}) {
     );
   }
 
-  if (badResults) {
-    return <div>Sem imagens para sugerir</div>;
-  }
-
   if (text) {
-    return (
-      <div>
-        <div>Imagens sugeridas</div>
-        <div className="grid grid-cols-3 sm:grid-cols-4 gap-1">
-          {images && images.map((image) => (
-            <img
-              alt="falha ao carregar"
-              src={image.thumbnail || image.original}
-              className="rounded-lg w-full aspect-square object-cover bg-gray-300 overflow-hidden"
-            />
-          ))}
+    if (images?.length) {
+      return (
+        <div>
+          <div>Imagens sugeridas</div>
+          <div className="grid grid-cols-3 sm:grid-cols-4 gap-1">
+            {images && images.map((image) => (
+              <img
+                alt="falha ao carregar"
+                src={image.thumbnail || image.original}
+                className="rounded-lg aspect-square object-cover bg-gray-300 overflow-hidden"
+              />
+            ))}
+          </div>
         </div>
-
-      </div>
-    );
+      );
+    }
+    return <div>Sem imagens para sugerir</div>;
   }
 
   return null;
