80b6bbc0a09ae20cdea680f3042f44217a40a7f8
Ulisses <m.stunik@gmail.com>
2022-02-12 12:43:14 -0300

    edit signin box

--- auth/SignInBox.tsx
+++ auth/SignInBox.tsx
@@ -6,11 +6,17 @@ import { GoogleButton } from './GoogleButton';
 export function SignInBox() {
   const { close } = useModal();
   return (
-    <div className="flex flex-col max-w-sm items-center gap-2 py-2">
-      <div className="pb-5">Escolha uma forma de continuar</div>
-      <GoogleButton callback={close} />
-      <FacebookButton callback={close} />
-      <div className="pb-2 sm:pb-5" />
+    <div className="flex flex-col max-w-sm items-center gap-4">
+      <div>
+        <div>
+          Escolha uma forma de continuar.
+        </div>
+        <strong>É toltamente grátis</strong>
+      </div>
+      <div className="flex flex-col gap-2">
+        <GoogleButton callback={close} />
+        <FacebookButton callback={close} />
+      </div>
       <div className="text-sm text-gray-600 px-6">
         Ao continuar, você concorda com nossa
         {' '}
@@ -21,7 +27,6 @@ export function SignInBox() {
         </Link>
         .
         {' '}
-        <strong>É toltamente grátis</strong>
       </div>
     </div>
   );
