f0a91de8802bbaf0dfd39a5e1b45e91a9b3b063c
Ulisses <m.stunik@gmail.com>
2022-02-16 09:05:27 -0300

    add new home page

--- home/NewHomePage.tsx
+++ home/NewHomePage.tsx
@@ -1,9 +1,43 @@
+import { FaChevronDown } from 'react-icons/fa';
+import { generateArray } from '../dev/utils/generateArray';
+
+const mockUsers = generateArray(30).map(() => ({
+  name: 'Muhammad Lee',
+  city: 'Serra Grande',
+  state: 'PB',
+}));
+
 export function NewHomePage() {
   return (
-    <div>
-      <div>
-        ulissse
+    <div className="p-2">
+      <div className="pb-6">
+        <div>Filtrar por</div>
+        <div className="flex flex-row gap-2 px-4">
+          <button disabled className="w-full disabled:bg-transparent disabled:text-gray-400">Cidade</button>
+          <button className="w-full ring-1">
+            Estado
+            <FaChevronDown size={16} />
+          </button>
+        </div>
+      </div>
+      <div className="pb-2">26 pessoas</div>
+      <div className="grid grid-cols-2 gap-2">
+        {mockUsers.map((user) => (
+          <button className="text-black flex flex-col gap-0 items-center bg-gray-100 p-2 rounded-lg">
+            <div>
+              {user.name}
+            </div>
+            <div>
+              {user.city}
+              {' '}
+              -
+              {' '}
+              {user.state}
+            </div>
+          </button>
+        ))}
       </div>
     </div>
+
   );
 }
