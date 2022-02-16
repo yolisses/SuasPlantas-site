5729783270ff59d178316e46d130f56f2bd11b9a
Ulisses <m.stunik@gmail.com>
2022-02-16 11:23:14 -0300

    styling

--- common/Header.tsx
+++ common/Header.tsx
@@ -40,7 +40,7 @@ export function Header() {
 
   return (
     <>
-      <div className="fixed flex flex-row pl-2 h-12 items-center w-full gap-2 z-50 bg-emerald-700 shadow-md">
+      <div className="fixed flex flex-row pl-2 h-12 items-center w-full gap-2 z-50 bg-emerald-600 shadow-md">
         <Link href="/">
           <a
             className="text-lg text-white cursor-pointer select-none hover:bg-green-500 hover:bg-opacity-30 p-2 rounded-lg"
