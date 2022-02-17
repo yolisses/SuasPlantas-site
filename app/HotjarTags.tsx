a765975ccc7707cbb52ede58c9958c61e7d9e383
Ulisses <m.stunik@gmail.com>
2022-02-17 17:43:20 -0300

    fix hotjar tags

--- app/HotjarTags.tsx
+++ app/HotjarTags.tsx
@@ -1,8 +1,11 @@
+import Head from 'next/head';
+
 export function HotjarTags() {
   //  Hotjar Tracking Code for https://suasplantas.com
   return (
-    <script>
-      {`(function(h,o,t,j,a,r){
+    <Head>
+      <script>
+        {`(function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
             h._hjSettings={hjid:2837294,hjsv:6};
             a=o.getElementsByTagName('head')[0];
@@ -10,6 +13,7 @@ export function HotjarTags() {
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
             a.appendChild(r);
         })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
-    </script>
+      </script>
+    </Head>
   );
 }
