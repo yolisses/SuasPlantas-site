4aeac6127f1538cf46abda07304ef81db7af6670
Ulisses <m.stunik@gmail.com>
2022-02-25 16:47:47 -0300

    add api location route

--- location/LocationFilterContext.tsx
+++ location/LocationFilterContext.tsx
@@ -1,6 +1,7 @@
 import {
   createContext, Dispatch, ReactNode, SetStateAction, useContext, useEffect, useState,
 } from 'react';
+import { api } from '../api/api';
 
 interface ILocationFilter{
     location?:string
@@ -13,10 +14,21 @@ interface ILocationFilter{
 export const locationContext = createContext({} as ILocationFilter);
 
 export function LocationFIlterContext({ children }:{children:ReactNode}) {
-  const [location, setLocation] = useState<Location>('teste');
+  const [location, setLocation] = useState<string>();
   const [coordinates, setCoordinates] = useState<[number, number]>([37.3931, -121.962]);
 
+  async function getLocationByIp() {
+    const ip = 2;
+    const res = await api.get('location');
+    const {
+      city, state, latitude, longitude,
+    } = res.data;
+    setLocation(`${city}, ${state}`);
+    setCoordinates([latitude, longitude]);
+  }
+
   useEffect(() => {
+    getLocationByIp();
   });
 
   return (
