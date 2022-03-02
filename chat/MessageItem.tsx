ef732f2d46809e39e20520b9dd36294a2b211665
Ulisses <m.stunik@gmail.com>
2022-03-02 15:23:37 -0300

    styling

--- chat/MessageItem.tsx
+++ chat/MessageItem.tsx
@@ -4,7 +4,7 @@ import { Message } from './Message';
 export function MessageItem({ message }:{message:Message}) {
   const { user } = useUser();
   return (
-    <div className={`py-1 px-2 rounded-lg ${
+    <div className={`py-1 px-2 rounded-lg break-all ${
       message.senderId === user?.id
         ? 'bg-green-100 self-end'
         : 'bg-gray-200 self-start'} `}
