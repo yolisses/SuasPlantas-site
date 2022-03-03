12e610bd6478a112a70ab0862d3b4b5223f24bff
Ulisses <m.stunik@gmail.com>
2022-03-03 13:57:52 -0300

    styling

--- chat/ChatItem.tsx
+++ chat/ChatItem.tsx
@@ -10,17 +10,18 @@ interface ChatButtonProps{
 export function ChatItem({ chat }:ChatButtonProps) {
   const imageSize = 60;
 
-  const { setCurrentChat } = useChats();
+  const { setCurrentChat, currentChat } = useChats();
 
   function handleClick() {
     setCurrentChat(chat);
   }
 
   return (
-
     <button
       onClick={handleClick}
-      className="highlight flex flex-row items-center gap-2 p-2 w-full rounded-md"
+      className={`highlight flex flex-row items-center gap-2 p-2 w-full rounded-md ${
+        chat.userId === currentChat?.userId ? 'bg-gray-100' : ''
+      }`}
     >
       <Image
         src={chat.image}
@@ -34,7 +35,9 @@ export function ChatItem({ chat }:ChatButtonProps) {
           { chat.name}
         </div>
         <div className="text-gray-500 overflow-hidden truncate">
-          { chat.text}
+          {!!chat.input && <span className="text-white bg-gray-500 rounded-lg p-1">falta enviar</span> }
+          {' '}
+          {chat.lastMessage?.text}
         </div>
       </div>
     </button>
