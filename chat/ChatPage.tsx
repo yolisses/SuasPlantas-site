2eb507ea7af24fb648cf277325fc0c5db2a41b4c
Ulisses <m.stunik@gmail.com>
2022-03-05 15:43:22 -0300

    refatorate

--- chat/ChatPage.tsx
+++ chat/ChatPage.tsx
@@ -1,9 +1,8 @@
 import { useRouter } from 'next/router';
-import { useSize } from '../size/SizeContext';
-import { Chat } from './Chat';
-import { ChatItem } from './ChatItem';
-import { ChatReader } from './ChatReader';
 import { useChats } from './ChatsContext';
+import { ChatReader } from './ChatReader';
+import { useSize } from '../size/SizeContext';
+import { ContactsPage } from './ContactsPage';
 
 export function ChatPage() {
   const { lg } = useSize();
@@ -11,21 +10,13 @@ export function ChatPage() {
   const { id } = useRouter().query;
 
   const chat = chats[id as string];
-  const values = Object.values(chats);
 
   return (
 
     <div className="h-screen-no-header flex flex-row overflow-hidden">
       {lg && (
-      <div className="w-full max-w-md overflow-y-auto shadow p-2 bg-white">
-        <div className="mb-2 text-lg">Conversas</div>
-        {values.length
-          ? values.map((item:Chat) => <ChatItem chat={item} />)
-          : (
-            <div className="pt-6 flex-1 flex items-center justify-center text-slate-600">
-              Sem conversas por enquanto
-            </div>
-          )}
+      <div className="w-full max-w-md overflow-y-auto shadow">
+        <ContactsPage />
       </div>
       )}
       <div className="flex flex-col w-full">
