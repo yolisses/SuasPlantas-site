cbd812bb2daddab7922a03bf85776d07c19e74d2
Ulisses <m.stunik@gmail.com>
2022-03-05 12:15:05 -0300

    add message date indicator

--- chat/MessagesList.tsx
+++ chat/MessagesList.tsx
@@ -24,7 +24,23 @@ export function MessagesList({ messages }:MessagesListProps) {
       <div
         className="flex flex-col-reverse justify-end p-2"
       >
-        {messages.map((item) => <MessageItem message={item} key={item.id} />)}
+        {messages.map((item, index, array) => {
+          let showDate = false;
+          const itemDate = (new Date(item.createdAt)).toLocaleDateString();
+          const next = array[index + 1];
+          if (next) {
+            const nextDate = (new Date(next.createdAt)).toLocaleDateString();
+            if (nextDate !== itemDate) showDate = true;
+          } else {
+            showDate = true;
+          }
+          return (
+            <>
+              <MessageItem message={item} key={item.id} />
+              { showDate && <div className="self-center bg-gray-100 p-2 rounded-lg">{itemDate}</div>}
+            </>
+          );
+        })}
       </div>
     </div>
   );
