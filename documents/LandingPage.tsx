f702261b8b70ea9100c2e79cb7e36db8944f3a4e
Ulisses <m.stunik@gmail.com>
2022-03-09 23:46:27 -0300

    iterate scratch

--- documents/LandingPage.tsx
+++ documents/LandingPage.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { FaRegCommentAlt, FaRegUser } from 'react-icons/fa';
 import { ShareButtons } from '../share/ShareButtons';
 
 export function LandingPage() {
@@ -17,9 +18,30 @@ export function LandingPage() {
       />
       <div className="p-2 flex flex-col gap-3">
         <h1 className="text-2xl">Encontre pessoas que gostam de plantas</h1>
-        <button className="main-button py-3 px-6 text-lg bg-gradient-to-r text-white from-green-500 to-emerald-500">
+        <button className="main-button text-lg bg-gradient-to-r text-white from-green-500 to-emerald-500">
+          <FaRegUser />
           Entrar
         </button>
+      </div>
+      <div>
+        <h2 className="text-xl flex flex-row items-center gap-2">
+          <FaRegCommentAlt size={25} />
+          <span>
+            Envie mensagens
+          </span>
+        </h2>
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum veniam ut quos fuga velit qui laudantium atque sint corrupti illo neque dolores odio, fugiat fugit voluptatem eius mollitia id cumque!
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum veniam ut quos fuga velit qui laudantium atque sint corrupti illo neque dolores odio, fugiat fugit voluptatem eius mollitia id cumque!
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum veniam ut quos fuga velit qui laudantium atque sint corrupti illo neque dolores odio, fugiat fugit voluptatem eius mollitia id cumque!
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum veniam ut quos fuga velit qui laudantium atque sint corrupti illo neque dolores odio, fugiat fugit voluptatem eius mollitia id cumque!
       </div>
       <div>
         <div>Compartilhe essa ideia</div>
@@ -30,6 +52,17 @@ export function LandingPage() {
           }}
           />
         </div>
+        <div className="sticky bottom-0 flex flex-col p-2 w-full bg-white">
+          <button className="main-button text-lg bg-gradient-to-r text-white from-green-500 to-emerald-500">
+            <FaRegUser />
+            Entrar
+          </button>
+        </div>
+      </div>
+      <div className="bg-slate-900 text-white">
+        <div>Links</div>
+        <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia blanditiis vitae possimus ipsam enim? Distinctio, nesciunt porro magni neque repellat laboriosam tempora ipsum quasi dolorum, optio sapiente veniam, nisi quae!</div>
+        <div />
       </div>
     </div>
   );
