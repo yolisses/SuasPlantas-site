4e43c56c36407a4e5bf0bffdbb840489ebb99855
Ulisses <m.stunik@gmail.com>
2022-03-10 16:16:19 -0300

    styling

--- documents/MessagesExample.tsx
+++ documents/MessagesExample.tsx
@@ -6,8 +6,8 @@ export function MessagesExample() {
     <div className="flex flex-col gap-2 w-64 p-2 text-sm lg:w-72 lg:text-base rounded-lg transform rotate-6">
       <div className="p-1 lg:p-2 shadow-lg rounded-lg ml-6 self-end bg-green-100">Oi, como vai?</div>
       <div className="p-1 lg:p-1 shadow-lg rounded-lg ml-6 self-end bg-green-100">
-        <div className="bg-black bg-opacity-10 flex flex-row">
-          <div>
+        <div className="bg-black bg-opacity-10 flex flex-row h-20 overflow-hidden">
+          <div className="">
             <Image
               src="/landing/thumb.jpg"
               height={imageSize}
@@ -15,9 +15,9 @@ export function MessagesExample() {
               objectFit="cover"
             />
           </div>
-          <div className="p-2 flex-1 h-20 overflow-y-hidden flex flex-col">
-            <div className="font-bold">Echeveria orion</div>
-            <div className="overflow-ellipsis flex-1 text-sm">Estou podando dessa suculenta, se alguém quiser, é dizer o endereço que eu envio por carta</div>
+          <div className="p-2 flex-1 flex flex-col">
+            <div className="font-bold overflow-ellipsis">Echeveria orion</div>
+            <div className="flex-1 text-sm overflow-ellipsis">Estou podando dessa suculenta, se alguém quiser, é dizer o endereço que eu envio por carta</div>
           </div>
         </div>
         <div className="lg:p-1">
