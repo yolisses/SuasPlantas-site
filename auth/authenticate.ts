f09663806594bda8d49cec004466b25c1dc2f14c
Ulisses <m.stunik@gmail.com>
2022-03-13 10:32:54 -0300

    remove console.log

--- auth/authenticate.ts
+++ auth/authenticate.ts
@@ -5,7 +5,6 @@ import { api } from '../api/api';
 
 export function authenticate(ctx:GetServerSidePropsContext<ParsedUrlQuery, PreviewData>) {
   const { 'suasplantas.token': token } = parseCookies(ctx);
-  console.log(token);
   if (token) { api.defaults.headers.common.Authorization = token; }
   return token || undefined;
 }
