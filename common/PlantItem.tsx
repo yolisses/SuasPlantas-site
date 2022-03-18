024cde5f0afb9e91097fc1e4f538e92627b70323
Ulisses <m.stunik@gmail.com>
2022-03-18 15:11:03 -0300

    styling

--- common/PlantItem.tsx
+++ common/PlantItem.tsx
@@ -32,8 +32,8 @@ export function PlantItem({
           className="bg-gray-200 flex-shrink-0 rounded-xl"
         />
         )}
-        <div className="px-2 pb-1 text-center overflow-hidden">
-          <span className={`overflow-hidden whitespace-nowrap ${
+        <div className="px-2 pb-1 text-center overflow-x-hidden text-ellipsis">
+          <span className={`overflow-x-hidden whitespace-nowrap ${
             !card
               ? 'text-2xl'
               : 'text-lg'}`}
