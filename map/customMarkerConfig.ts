12f5074ac55892520ebbaa5256460e0561e2291d
Ulisses <m.stunik@gmail.com>
2022-03-19 23:25:50 -0300

    refatorate

--- map/customMarkerConfig.ts
+++ map/customMarkerConfig.ts
@@ -1,12 +1,12 @@
 import { IconOptions } from 'leaflet';
 
-const iconWidth = 32.8;
-const iconHeight = 48;
+export const markerWidth = 32.8;
+export const markerHeight = 48;
 
 export const customMarkerConfig:IconOptions = {
   iconUrl: '/map/marker.svg',
-  iconSize: [iconWidth, iconHeight],
-  popupAnchor: [0, -iconHeight],
-  iconAnchor: [iconWidth / 2, iconHeight],
-  tooltipAnchor: [0, -iconHeight / 2],
+  popupAnchor: [0, -markerHeight],
+  tooltipAnchor: [0, -markerHeight / 2],
+  iconSize: [markerWidth, markerHeight],
+  iconAnchor: [markerWidth / 2, markerHeight],
 };
