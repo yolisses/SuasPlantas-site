f2464950a590f3992d70e04e7182f248f1e9b752
Ulisses <m.stunik@gmail.com>
2022-03-20 11:57:45 -0300

    fix profile button

--- footer/ProfileThumb.tsx
+++ footer/ProfileThumb.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { userImage } from '../images/user';
 import { useUser } from '../auth/userContext';
 
@@ -6,12 +7,14 @@ export function ProfileThumb() {
   const { user } = useUser();
   const imageSize = 24;
   return (
-    <Image
-      objectFit="cover"
-      width={imageSize}
-      height={imageSize}
-      src={user?.image || userImage}
-      className="rounded-full border-white border-2"
-    />
+    <Link href={user ? `/users/${user?.id}` : '/landing'}>
+      <Image
+        objectFit="cover"
+        width={imageSize}
+        height={imageSize}
+        src={user?.image || userImage}
+        className="rounded-full border-white border-2"
+      />
+    </Link>
   );
 }
