286a1815e8ac79cf3f3c772cbf3886597b6dc58a
Ulisses <m.stunik@gmail.com>
2022-03-21 15:28:49 -0300

    fix footer

--- footer/Footer.tsx
+++ footer/Footer.tsx
@@ -1,6 +1,6 @@
 import { useSize } from '../common/SizeContext';
-import { FooterNavigationItem } from './FooterNavigationItem';
 import { navigationItems } from './NavigationItems';
+import { FooterNavigationItem } from './FooterNavigationItem';
 
 interface FooterProps{
     selected?:string
@@ -14,11 +14,10 @@ export function Footer({ selected }:FooterProps) {
       <div className="h-12 relative" />
       <div
         style={{ boxShadow: '0 0 4px #0008' }}
-        className="h-12 bg-white sticky w-full bottom-0 flex flex-row items-center justify-around"
+        className="h-12 bg-white fixed w-full bottom-0 flex flex-row items-center justify-around"
       >
         {navigationItems.map((item) => <FooterNavigationItem key={item.id} item={item} selected={item.id === selected} />)}
       </div>
     </>
-
   );
 }
