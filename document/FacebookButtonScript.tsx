48418cdc7451c67a3881ce1baebd4b9e44692606
Ulisses <m.stunik@gmail.com>
2022-03-22 13:10:19 -0300

    lint

--- document/FacebookButtonScript.tsx
+++ document/FacebookButtonScript.tsx
@@ -1,8 +1,5 @@
 export function FacebookButtonScript() {
   return (
-    <script
-      async
-      src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v12.0&appId=256482496640231&autoLogAppEvents=1"
-    />
+    <script async src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v12.0&appId=256482496640231&autoLogAppEvents=1" />
   );
 }
--- document/FaviconMeta.tsx
+++ document/FaviconMeta.tsx
@@ -18,14 +18,17 @@ export function FaviconMeta() {
         sizes="16x16"
         href="/favicon/favicon-16x16.png"
       />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link
+        rel="manifest"
+        href="/favicon/site.webmanifest"
+      />
       <link
         rel="mask-icon"
         href="/favicon/safari-pinned-tab.svg"
         color="#5bbad5"
       />
-      <meta name="msapplication-TileColor" content="#00a300" />
       <meta name="theme-color" content="#ffffff" />
+      <meta name="msapplication-TileColor" content="#00a300" />
     </>
   );
 }
