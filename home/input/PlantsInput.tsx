0eb329307201cedc9c8a5e17697a25e5b11d1cf6
Ulisses <m.stunik@gmail.com>
2022-03-28 11:51:39 -0300

    refatorate

--- home/input/PlantsInput.tsx
+++ home/input/PlantsInput.tsx
@@ -1,38 +1,43 @@
 import Image from 'next/image';
 import { useUser } from '../../auth/userContext';
+import { useSize } from '../../common/SizeContext';
 import { userImageSVG } from '../../images/user';
 import { Chip } from './Chip';
 
 export function PlantsInput() {
-  const { user } = useUser();
   const imageSize = 60;
   const maxPlants = 10;
+  const { md } = useSize();
+  const { user } = useUser();
+
   return (
     <div className="p-2 rounded-xl bg-gray-100">
-      <div className="grid grid-cols-2 gap-2">
-        <div className="flex flex-col gap-2">
+      <div className="flex flex-row gap-2">
+        <div className="flex flex-col gap-2 flex-1">
           <div className="text-lg">Quais plantas você tem?</div>
           <div className="gap-2 center-row">
-            <Image
-              objectFit="cover"
-              width={imageSize}
-              height={imageSize}
-              className="rounded-full"
-              src={user?.image || userImageSVG}
-            />
             <div>
-              <input type="text" />
+              <Image
+                objectFit="cover"
+                width={imageSize}
+                height={imageSize}
+                className="rounded-full"
+                src={user?.image || userImageSVG}
+              />
             </div>
+            <input type="text" className="flex-1" />
           </div>
         </div>
-        <div className="flex flex-row flex-wrap h-fit gap-1 overflow-hidden">
-          {user?.plants.slice(0, maxPlants).map((plant) => (
-            <Chip text={plant.name} image={plant.card} />
+        {md && (
+        <div className="flex-1 flex flex-row flex-wrap h-fit gap-1 overflow-hidden max-w-md">
+          {user?.plants.slice(0, maxPlants).map(({ name, card }) => (
+            <Chip text={name} image={card} />
           ))}
           {user && user.plants.length > maxPlants && (
           <Chip text={`+${user.plants.length - maxPlants} outras`} />
           )}
         </div>
+        )}
       </div>
     </div>
 
