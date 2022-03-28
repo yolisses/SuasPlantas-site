5ce62fce099f153de6b9be22ebdd980a03b4f2b3
Ulisses <m.stunik@gmail.com>
2022-03-28 13:59:59 -0300

    add images field on plants input

--- home/input/PlantsInput.tsx
+++ home/input/PlantsInput.tsx
@@ -1,13 +1,20 @@
 import Image from 'next/image';
+import { FaImage } from 'react-icons/fa';
+import { useState } from 'react';
 import { useUser } from '../../auth/userContext';
 import { userImageSVG } from '../../images/user';
 import { useSize } from '../../common/SizeContext';
-import { UserPlantsReminder } from './UserPlantsReminder';
+import { ImagesInput } from '../../images/ImagesInput';
 
 export function PlantsInput() {
   const imageSize = 60;
   const { md } = useSize();
   const { user } = useUser();
+  const [showImages, setShowImages] = useState(false);
+
+  function handleImagesClick() {
+    setShowImages((value) => !value);
+  }
 
   let firstName;
   if (user) { firstName = ` ${user.name.split(' ')[0]}`; }
@@ -32,17 +39,23 @@ export function PlantsInput() {
                 src={user?.image || userImageSVG}
               />
             </div>
-            <form className="flex-1 flex flex-col gap-2">
+            <div className="flex-1 flex flex-col gap-2">
               <input
                 type="text"
-                className="flex-1 placeholder-gray-500 bg-white p-3 rounded-xl"
+                className="placeholder-gray-500 bg-white p-3 rounded-lg"
                 placeholder="Nome da planta"
               />
-              <input type="submit" value="Adicionar" className="main-button max-w-sm" />
-            </form>
+              {showImages && <ImagesInput onChange={() => {}} />}
+              <div className="center-row gap-2">
+                <button className="icon-button" onClick={handleImagesClick}>
+                  <FaImage size={20} color="#080" />
+                </button>
+                <input type="submit" value="Adicionar" className="main-button max-w-sm" />
+              </div>
+            </div>
           </div>
         </div>
-        {md && <UserPlantsReminder />}
+        {/* {md && <UserPlantsReminder />} */}
       </div>
     </div>
 
