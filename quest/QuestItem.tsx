1c62fc2d0e2ebf6d0fce41774de10e3ea53167fb
Ulisses <m.stunik@gmail.com>
2022-03-29 16:28:20 -0300

    styling

--- quest/QuestItem.tsx
+++ quest/QuestItem.tsx
@@ -4,30 +4,29 @@ import { userImage } from '../images/user';
 import { Quest } from './Quest';
 
 export function QuestItem({ item }:{item:Quest}) {
+  const imageSize = 30;
   const user = item?.user;
   return (
     <Link href={`users/${user?.id}`}>
-      <a tabIndex={-1}>
-        <button className="center p-2 w-full rounded-lg text-black bg-gray-200 hover:bg-gray-300 text-center shadow-md flex flex-col gap-2 highlight">
-          <div className="text-lg">
-            {item.name}
-            {' '}
-          </div>
-          {!!user && (
+      <button className="center-col p-2 rounded-lg shadow gap-2 bg-gray-100 highlight">
+        <div className="text-lg">{item.name}</div>
+        {!!user && (
           <div className="flex flex-row gap-2 items-center">
-            <Image
-              width={30}
-              height={30}
-              objectFit="cover"
-              layout="intrinsic"
-              src={user.image || userImage}
-              className="rounded-full flex-nowrap shrink-0"
-            />
-            <div className="flex-1 overflow-hidden text-sm">
-              <div className="text-left font-semibold overflow-hidden text-ellipsis whitespace-nowrap">
+            <div className="flex-shrink-0">
+              <Image
+                objectFit="cover"
+                width={imageSize}
+                height={imageSize}
+                layout="intrinsic"
+                src={user.image || userImage}
+                className="rounded-full shrink-0"
+              />
+            </div>
+            <div className="flex-1 text-sm overflow-hidden text-ellipsis whitespace-nowrap">
+              <div className="font-semibold overflow-hidden text-ellipsis whitespace-nowrap">
                 {user.name}
               </div>
-              <div className="text-left overflow-hidden text-ellipsis whitespace-nowrap">
+              <div className="overflow-hidden text-ellipsis whitespace-nowrap">
                 {user.city}
                 ,
                 {' '}
@@ -35,9 +34,8 @@ export function QuestItem({ item }:{item:Quest}) {
               </div>
             </div>
           </div>
-          )}
-        </button>
-      </a>
+        )}
+      </button>
     </Link>
   );
 }
