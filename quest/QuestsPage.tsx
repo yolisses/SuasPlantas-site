ad657dc5eca85fb587dacf9acb5cf49e7f2406e6
Ulisses <m.stunik@gmail.com>
2022-03-29 11:21:19 -0300

    lint

--- quest/QuestsPage.tsx
+++ quest/QuestsPage.tsx
@@ -1,4 +1,3 @@
-import { Quest } from './Quest';
 import { QuestItem } from './QuestItem';
 import { HomePage } from '../home/HomePage';
 import { questsContext } from './questsContext';
@@ -11,7 +10,7 @@ export function QuestsPage() {
     >
       {(items) => (
         <div className="p-2 grid gap-2 grid-cols-2 md:grid-cols-4 xl:grid-cols-5">
-          {!!items && items.map((item: Quest) => (
+          {!!items && items.map((item) => (
             <QuestItem item={item} key={item.id} />
           ))}
         </div>
