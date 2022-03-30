bda538dbc75329f7051c50c83e5ed2805a6d07b9
Ulisses <m.stunik@gmail.com>
2022-03-30 12:04:41 -0300

    change icon on pwa and favicon

--- document/PWAMeta.tsx
+++ document/PWAMeta.tsx
@@ -2,7 +2,7 @@ export function PWAMeta() {
   return (
     <>
       <meta name="theme-color" content="#fff" />
-      <link rel="manifest" href="/manifest.json" />
+      <link rel="manifest" href="/pwa/manifest.json" />
       <link rel="apple-touch-icon" href="/icon.png" />
     </>
   );
