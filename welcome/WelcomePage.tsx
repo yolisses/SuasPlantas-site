30c4127d463e343e90af84587187a99c1ab3c0ce
Ulisses <m.stunik@gmail.com>
2022-03-31 20:53:32 -0300

    add form hook on welcome page

--- welcome/WelcomePage.tsx
+++ welcome/WelcomePage.tsx
@@ -1,10 +1,22 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import Image from 'next/image';
+import { useForm } from 'react-hook-form';
 import { WelcomeInput } from './WelcomeInput';
 import { useVocative } from '../home/input/useVocative';
 
+interface WelcomeFormData{
+  plants:string[]
+  quests:string[]
+}
+
 export function WelcomePage() {
   const { vocative } = useVocative();
+
+  const { register, handleSubmit } = useForm<WelcomeFormData>();
+
+  function submit(data:WelcomeFormData) {
+    console.log(data);
+  }
 
   const imageSize = 24;
   return (
@@ -13,17 +25,22 @@ export function WelcomePage() {
         <Image src="/icon/icon.svg" width={imageSize} height={imageSize} />
         SuasPlantas
       </div>
-      <div className="p-3 flex flex-col gap-4 text-lg w-full max-w-3xl flex-1">
+      <form
+        onSubmit={handleSubmit(submit)}
+        className="p-3 flex flex-col gap-4 text-lg w-full max-w-3xl flex-1"
+      >
         <h1 className="text-2xl">
           Seja bem vindo(a)
           {vocative}
         </h1>
         <h3 className="text-gray-500">Só duas perguntinhas para deixarmos tudo pronto...</h3>
-        <WelcomeInput type="plants" />
-        <WelcomeInput type="quests" />
+        <WelcomeInput type="plants" {...register('plants')} />
+        <WelcomeInput type="quests" {...register('quests')} />
         <div className="flex-1" />
-        <button className="secondary-button">Prosseguir</button>
-      </div>
+        <button className="secondary-button">
+          Prosseguir
+        </button>
+      </form>
     </div>
   );
 }
