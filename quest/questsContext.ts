d0285b4095150352ed92cabb096ddadfae69f174
Ulisses <m.stunik@gmail.com>
2022-04-01 21:51:17 -0300

    remove quest

--- quest/questsContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react';
-import { ItemsContext } from '../pagination/PaginationProvider';
-import { Quest } from './Quest';
-
-export const questsContext = createContext({} as ItemsContext<Quest>);
-
-export function useQuests() {
-  return useContext(questsContext);
-}
